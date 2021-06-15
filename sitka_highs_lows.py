07b0ff68aa7a9cc61b20edc2613089d6423a517c
Yasir Ahmed <ahmedyasir77@gmail.com>
2021-06-15 17:41:46 -0500

    Plotting a longer timeframe

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -4,22 +4,27 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 filename = 'data/sitka_weather_2018_simple.csv'
+
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+
+    # Get dates, and high and low temperatures from this file.
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperatures.
+# Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c = 'red')
+ax.plot(dates, highs, c = 'red', alpha = 0.5)
+ax.plot(dates, lows, c = 'blue', alpha = 0.5)
+ax.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1)
 
 # Formate plot.
 ax.set_title("Daily high temperatures - 2018", fontsize = 24)
