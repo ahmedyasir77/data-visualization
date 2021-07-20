180df269d3dfe543024f5949a88b42467bdf5bac
Yasir Ahmed <ahmedyasir77@gmail.com>
2021-07-19 21:36:48 -0500

    Map change

--- rw_visual.py
+++ rw_visual.py
@@ -12,11 +12,11 @@ while True:
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize = (15, 9))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap=plt.cm.Blues, edgecolors = 'none', s = 1)
+    ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap=plt.cm.Blues, edgecolors = '', s = 1)
     
     # Emphasize the first and last points.
     ax.scatter(0, 0, c = 'green', edgecolors = 'none', s = 100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolors = 'none', s = 100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolors = '', s = 100)
     
     # Remove the axes.
     ax.get_xaxis().set_visible(False)
